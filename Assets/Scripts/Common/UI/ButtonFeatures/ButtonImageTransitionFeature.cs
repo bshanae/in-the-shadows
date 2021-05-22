358d9a99f3fd606a34ce4d617a9150397254ae64
v.belchenko <belchenkovova@gmail.com>
2021-05-22 09:47:21 +0300

    Added exit button in level menu

--- Assets/Scripts/Common/UI/ButtonFeatures/ButtonImageTransitionFeature.cs
+++ Assets/Scripts/Common/UI/ButtonFeatures/ButtonImageTransitionFeature.cs
@@ -6,35 +6,26 @@ namespace Common.UI
 {
 	[RequireComponent(typeof(Button))]
 	[RequireComponent(typeof(Image))]
-	public class ButtonToggleFeature : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+	public class ButtonImageTransitionFeature : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 	{
 		[SerializeField] private Sprite defaultSprite;
 		[SerializeField] private Sprite pressedSprite;
 
 		private Image _image;
 
-		public bool State { get; private set; }
-
 		private void Awake()
 		{
 			_image = GetComponent<Image>();
-			State = true;
 		}
 
 		public void OnPointerDown(PointerEventData eventData)
 		{
-			State = !State;
-
 			_image.sprite = pressedSprite;
-			_image.color = Settings.Instance.buttonToggleFeature.onColor;
 		}
 
 		public void OnPointerUp(PointerEventData eventData)
 		{
 			_image.sprite = defaultSprite;
-
-			if (!State)
-				_image.color = Settings.Instance.buttonToggleFeature.offColor;
 		}
 	}
 }
